d9a42c141d8a9ff787389c77e0fe17da71969526
stefan-matcovici <stefan.matcovici@yahoo.com>
2018-02-20 11:31:40 +0200

    Implemented Strassen with classes and with not power of two order matrix

---
+++ Homework1/Matrix.py
@@ -0,0 +1,105 @@
+import math
+
+
+class Matrix:
+    def __init__(self, matrix):
+        self.size = len(matrix)
+        self.data = matrix
+        self.unpad_size = self.size
+
+    def __add__(self, other):
+        if other.size != self.size:
+            raise ValueError("Invalid matrix to add")
+        return Matrix([[self.data[i][j] + other.data[i][j] for j in range(self.size)] for i in range(self.size)])
+
+    def __sub__(self, other):
+        if other.size != self.size:
+            raise ValueError("Invalid matrix to sub")
+        return Matrix([[self.data[i][j] - other.data[i][j] for j in range(self.size)] for i in range(self.size)])
+
+    def __matmul__(self, other):
+        half = self.size // 2
+        if half == 1:
+            return self * other
+
+        self.pad()
+
+        a = self.slice(half)
+        b = other.slice(half)
+
+        p1 = (a[0][0] + a[1][1]) @ (b[0][0] + b[1][1])
+        p2 = (a[1][0] + a[1][1]) @ b[0][0]
+        p3 = a[0][0] @ (b[0][1] - b[1][1])
+        p4 = a[1][1] @ (b[1][0] - b[0][0])
+        p5 = (a[0][0] + a[0][1]) @ b[1][1]
+        p6 = (a[1][0] - a[0][0]) @ (b[0][0] + b[0][1])
+        p7 = (a[0][1] - a[1][1]) @ (b[1][0] + b[1][1])
+
+        c11 = p1 + p4 - p5 + p7
+        c12 = p3 + p5
+        c21 = p2 + p4
+        c22 = p1 + p3 - p2 + p6
+
+        self.recompose(c11, c12, c21, c22)
+        self.unpad()
+
+        return self
+
+    def __len__(self):
+        return self.size
+
+    def __mul__(self, other):
+        r = []
+        m = []
+        for i in range(self.size):
+            for j in range(self.size):
+                sums = 0
+                for k in range(self.size):
+                    sums = sums + (self.data[i][k] * other.data[k][j])
+                r.append(sums)
+            m.append(r)
+            r = []
+        return Matrix(m)
+
+    def __str__(self):
+        return str(self.data)
+
+    def slice(self, size):
+        return [[Matrix([x[:size] for x in self.data[:size]]),
+                 Matrix([x[size:] for x in self.data[:size]])],
+                [Matrix([x[:size] for x in self.data[size:]]),
+                 Matrix([x[size:] for x in self.data[size:]])]]
+
+    def recompose(self, c11, c12, c21, c22):
+        size = len(c11)
+        for i in range(size):
+            for j in range(size):
+                c11.data[i].append(c12.data[i][j])
+                c21.data[i].append(c22.data[i][j])
+
+        for i in range(size):
+            c11.data.append(c21.data[i])
+
+        self.data = c11.data
+
+    def next_power_of_2(self):
+        return 1 if self.size == 0 else 2 ** math.ceil(math.log2(self.size))
+
+    def pad(self):
+        self.size = self.next_power_of_2()
+        pad_blank_line1 = [0] *self.size
+        pad_blank_line2 = [0] * (self.size - self.unpad_size)
+
+        for i in range(self.unpad_size):
+            self.data[i][self.unpad_size:] = pad_blank_line2
+
+        for i in range(self.size - self.unpad_size):
+            self.data.append(pad_blank_line1)
+
+    def unpad(self):
+        for i in range(self.size):
+            self.data[i] = self.data[i][:self.unpad_size]
+
+        self.size = self.unpad_size
+        self.data = self.data[:self.size]
+
--- Homework1/main.py
+++ Homework1/main.py
@@ -1,10 +1,16 @@
 import problem1
 import problem2
 import problem3
+from Matrix import Matrix
 
 matrix1 = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
 matrix2 = [[11, 12, 13, 14], [15, 16, 17, 18], [19, 20, 21, 22], [23, 24, 25, 26]]
 
+matrix3 = [[1, 2, 3], [5, 6, 7], [9, 10, 11]]
+matrix4 = [[11, 12, 13], [15, 16, 17], [19, 20, 21]]
+
 print(problem1.get_machine_precision())
 print(problem2.get_nonassociative_numbers())
 print(problem3.strassen_multiplication(matrix1, matrix2))
+print(Matrix(matrix1) @ Matrix(matrix2))
+print(Matrix(matrix3) @ Matrix(matrix4))
