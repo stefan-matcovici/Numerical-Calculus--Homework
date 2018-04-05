69ee12507a789052fab48d33d86c8728b8d52e05
andrluc <andrluc@amazon.com>
2018-04-05 13:28:04 +0300

    Added interpolation generation functions.

--- Homework7/main.py
+++ Homework7/main.py
@@ -1,8 +1,30 @@
 from Polynomial import Polynomial
 
-x0 = 0
-xn = 10
-n = 10
+x0 = 1
+xn = 5
+n = 4
 
 p = Polynomial('../test/poli.txt')
-print(p.get_result_horner(1.5))
+
+
+def f1(value, function):
+    mapping = dict()
+    h = (xn - x0) / n
+
+    for i in range(n):
+        mapping[x0 + i * h] = function(x0 + i * h)
+
+    return mapping[value]
+
+
+def f2(value):
+    mapping = dict()
+    mapping[0] = 50
+    mapping[1] = 47
+    mapping[2] = -2
+    mapping[3] = -121
+    mapping[4] = -310
+    mapping[5] = -545
+
+    return mapping[value]
+
