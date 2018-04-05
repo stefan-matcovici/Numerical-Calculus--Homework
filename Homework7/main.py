64d0660686f3912751ef452962070a0189f815e9
andrluc <andrluc@amazon.com>
2018-04-05 16:43:20 +0300

    Finished the Lagrange interpolation function(not working.)

--- Homework7/main.py
+++ Homework7/main.py
@@ -1,20 +1,26 @@
 from Polynomial import Polynomial
 
-x0 = 1
+# x0 = 1
+# xn = 5
+# n = 4
+# h = (xn - x0) / n
+#
+# p = Polynomial('../test/poli.txt')
+#
+#
+# def f1(value, function):
+#     mapping = dict()
+#
+#     for i in range(n):
+#         mapping[x0 + i * h] = function(x0 + i * h)
+#
+#     return mapping[value]
+
+
+x0 = 0
 xn = 5
-n = 4
-
-p = Polynomial('../test/poli.txt')
-
-
-def f1(value, function):
-    mapping = dict()
-    h = (xn - x0) / n
-
-    for i in range(n):
-        mapping[x0 + i * h] = function(x0 + i * h)
-
-    return mapping[value]
+n = 5
+h = (xn - x0) / n
 
 
 def f2(value):
@@ -28,3 +34,45 @@ def f2(value):
 
     return mapping[value]
 
+
+def get_Lagrange_interpolation_value(value, value_function):
+    t = (value - x0) / h
+    y0 = value_function(x0)
+
+    deltas = get_delta_f_x0(value_function)
+    sks = get_sks(t)
+
+    lagrange_value = y0
+    for i in range(n):
+        lagrange_value += deltas[i] * sks[i]
+
+    return lagrange_value
+
+
+def get_delta_f_x0(value_function):
+    deltas = [value_function(x0)]
+
+    for i in range(n):
+        deltas.append(value_function(x0 + (i + 1) * h))
+
+    for i in range(n):
+        for j in range(n, i, -1):
+            deltas[j] = deltas[j] - deltas[j - 1]
+
+    return deltas
+
+
+def get_sks(t):
+    sks = [t]
+
+    for k in range(1, n):
+        sks.append(sks[k - 1] * (t - k + 1) / k)
+
+    return sks
+
+
+def get_least_squares_value(value):
+    pass
+
+
+print(get_Lagrange_interpolation_value(1.5, f2))
