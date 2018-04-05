def21e7d0b34ed2d27c1c09e5fc208a15ca04f8e
DESKTOP-L8H23GJ\Luca Andrei <luca.andrei96@gmail.com>
2018-04-05 11:27:41 +0300

    Started working on Homework 7

---
+++ Homework7/main.py
@@ -0,0 +1,8 @@
+from Polynomial import Polynomial
+
+x0 = 0
+xn = 10
+n = 10
+
+p = Polynomial('../test/poli.txt')
+print(p.get_result_horner(1.5))
--- Homework8/main.py
+++ Homework8/main.py
@@ -1,6 +1,7 @@
 from Polynomial import Polynomial
 
 p = Polynomial('../test/poli.txt')
+
 print(p.approximate_roots(0.1, retries=10, kmax=100, epsilon=0.000000001, f=p.get_result_horner))
 print([x if p.get_second_derivative_value(float(x)) > 0 else None for x in p.approximate_roots(0.1, retries=2, kmax=100, epsilon=0.000000001, f=p.get_derivative_value1)])
 print([x if p.get_second_derivative_value(float(x)) > 0 else None for x in p.approximate_roots(0.1, retries=2, kmax=100, epsilon=0.000000001, f=p.get_derivative_value2)])
