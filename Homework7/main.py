56a982213d1db3b531ae05b7757235f33c7cf5ea
andrluc <andrluc@amazon.com>
2018-04-05 17:21:08 +0300

    Added dumb functional Lagrange function and added graph plotting.

--- Homework7/main.py
+++ Homework7/main.py
@@ -1,5 +1,6 @@
-from Polynomial import Polynomial
-
+import matplotlib.pyplot as plt
+# from Polynomial import Polynomial
+#
 # x0 = 1
 # xn = 5
 # n = 4
@@ -15,7 +16,7 @@ from Polynomial import Polynomial
 #         mapping[x0 + i * h] = function(x0 + i * h)
 #
 #     return mapping[value]
-
+import numpy as np
 
 x0 = 0
 xn = 5
@@ -35,6 +36,18 @@ def f2(value):
     return mapping[value]
 
 
+def get_Lagrange_interpolation_value_dumb(value, value_function):
+    sum = 0
+    for i in range(n):
+        product = 1
+        for j in range(n):
+            if i == j:
+                continue
+            product *= (value - get_x(j)) / (get_x(i) - get_x(j))
+        sum += product * value_function(get_x(i))
+
+    return sum
+
 def get_Lagrange_interpolation_value(value, value_function):
     t = (value - x0) / h
     y0 = value_function(x0)
@@ -53,7 +66,7 @@ def get_delta_f_x0(value_function):
     deltas = [value_function(x0)]
 
     for i in range(n):
-        deltas.append(value_function(x0 + (i + 1) * h))
+        deltas.append(value_function(get_x(i)))
 
     for i in range(n):
         for j in range(n, i, -1):
@@ -62,11 +75,15 @@ def get_delta_f_x0(value_function):
     return deltas
 
 
+def get_x(i):
+    return x0 + (i + 1) * h
+
+
 def get_sks(t):
     sks = [t]
 
     for k in range(1, n):
-        sks.append(sks[k - 1] * (t - k + 1) / k)
+        sks.append(sks[k - 1] * ((t - k + 1) / k))
 
     return sks
 
@@ -74,5 +91,11 @@ def get_sks(t):
 def get_least_squares_value(value):
     pass
 
+xs = np.arange(x0, xn, h)
+ys = [f2(x) for x in xs]
+plt.plot(xs, ys, linewidth=2.0)
 
-print(get_Lagrange_interpolation_value(1.5, f2))
+xs = np.arange(x0, xn, h / 100)
+ys = [get_Lagrange_interpolation_value_dumb(x, f2) for x in xs]
+plt.plot(xs, ys, linewidth=2.0)
+plt.show()
